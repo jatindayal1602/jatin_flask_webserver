06e86b1f69dad598eae36a904bc2ff727920bd84
jatindayal1602 <jatin.dayal@igtsolutions.com>
2025-02-14 22:54:04 +0530

    somechanges in text

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
  
 @app.route('/')
 def home():
-    return "Hello, Flask Web Server running in Docker! by jatin dayal"
+    return "Hello, Flask Web Server running in Docker! by jatin dayal it is running on port 5000"
  
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
